6249e7a0c64ff3fa33d5665b6f2b368ead1430da
Ruhi Tamanna Alam <ruhialamsithy@gmail.com>
2017-07-18 05:08:38 +0000

    lecture 20

--- routes/product.server.routes.js
+++ routes/product.server.routes.js
@@ -3,6 +3,10 @@ module.exports = function(app){
  var products = require('./../controllers/products.server.controller.js');
  var users = require('./../controllers/users.server.controller.js');
 
+ app.route('/products/new')
+	.get(products.new);
+ 
+ 
  app.route('/api/products')
 	.get(products.list)
 	.post(users.requiresLogin, products.create);
